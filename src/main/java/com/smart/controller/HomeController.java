4d94008424202402f1980f3445f30db688c7dd0c
Shivansh Singh <shivanshsingh9670@gmail.com>
2024-07-25 07:55:58 +0530

    User Registration Process with Message

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -1,11 +1,24 @@
 package com.smart.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+import com.smart.helper.Message;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping("/home")
     public String Home(Model model) {
         model.addAttribute("title", "Home - Smart Contact Manager");
@@ -18,9 +31,44 @@ public class HomeController {
         return "about";
     }
 
-    @RequestMapping("/signup/")
+    @RequestMapping("/signup")
     public String Signup(Model model) {
         model.addAttribute("title", "Register - Smart Contact Manager");
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
+    @RequestMapping(value = "/do_register", method = RequestMethod.POST)
+    public String RegisterUser(@ModelAttribute("user") User user,
+            @RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model,
+            HttpSession session) {
+
+        try {
+            if (!agreement)
+                throw new Exception("You have not agreed to terms and conditions");
+
+            user.setRole("Role");
+            user.setEnabled(true);
+
+            System.out.println(user);
+
+            User result = this.userRepository.save(user);
+
+            model.addAttribute("user", new User());
+
+            session.setAttribute("message", new Message("Successfully registered", "alert-success"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("user", user);
+            session.setAttribute("message", new Message("something went wrong " + e.getMessage(), "alert-danger"));
+        }
+
+        if (session.getAttribute("message") != null) {
+            model.addAttribute("message", session.getAttribute("message"));
+            session.removeAttribute("message"); // Remove the message from the session
+        }
+
         return "signup";
     }
 }
