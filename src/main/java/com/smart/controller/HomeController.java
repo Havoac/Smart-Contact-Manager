dfa8960e6d0371c4f0f9888e166261c80cebe704
Shivansh Singh <shivanshsingh9670@gmail.com>
2024-07-27 13:46:35 +0530

    Revert "Server side validation"
    This reverts commit 8ff9d4a826fb79f9e2e131cb649e1551ac13bc4c.

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -3,7 +3,6 @@ package com.smart.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +13,6 @@ import com.smart.entities.User;
 import com.smart.helper.Message;
 
 import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
 
 @Controller
 public class HomeController {
@@ -41,18 +39,13 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/do_register", method = RequestMethod.POST)
-    public String RegisterUser(@Valid @ModelAttribute("user") User user, BindingResult bindingResult,
+    public String RegisterUser(@ModelAttribute("user") User user,
             @RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model,
             HttpSession session) {
 
         try {
             if (!agreement)
                 throw new Exception("You have not agreed to terms and conditions");
-
-            if (bindingResult.hasErrors()) {
-                model.addAttribute("user", user);
-                return "signup";
-            }
 
             user.setRole("Role");
             user.setEnabled(true);
