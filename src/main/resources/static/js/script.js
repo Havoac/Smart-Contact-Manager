0a5a9d43ee33a64bc5bb441ceffdd26183c12293
Shivansh Singh <shivanshsingh9670@gmail.com>
2024-07-29 08:38:08 +0530

    Creating Responsive Sidebar

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,12 @@
-console.log("this is js script");
+const toggleSidebar = () => {
+    if ($(".sidebar").is(":visible")) {
+        // if the sidebar is visible, then we need to close it
+        $(".sidebar").css("display", "none");
+        $(".content").css("margin-left", "0%");
+    }
+    else {
+        // if the sidebar is not visible, then we need to make it visible
+        $(".sidebar").css("display", "block");
+        $(".content").css("margin-left", "22%");
+    }
+};
