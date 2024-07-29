e947b6a6bb050906710cca002316942f3a090f44
Shivansh Singh <shivanshsingh9670@gmail.com>
2024-07-29 19:59:05 +0530

    Design Add Contact Form

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -5,9 +5,11 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smart.dao.UserRepository;
+import com.smart.entities.Contact;
 import com.smart.entities.User;
 
 @Controller
@@ -16,14 +18,29 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/index")
-    public String dashboard(Model model, Principal principal) {
+    // this will be available for all the pages
+    // response method for adding common data
+    @ModelAttribute
+    public void addCommonData(Model model, Principal principal) {
         String userName = principal.getName(); // username - email
 
         User user = userRepository.getUserByUserName(userName);
 
         model.addAttribute("user", user);
+    }
 
+    // dashboard home
+    @RequestMapping("/index")
+    public String dashboard(Model model, Principal principal) {
+        model.addAttribute("title", "User Dashboard");
         return "normal/user_dashboard";
+    }
+
+    // open add form controller
+    @RequestMapping("/add-contact")
+    public String openAddContactForm(Model model) {
+        model.addAttribute("title", "Add Contact");
+        model.addAttribute("contact", new Contact());
+        return "normal/add_contact_form";
     }
 }
