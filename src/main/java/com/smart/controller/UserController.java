1ec0d105be19022186325d3d71d20caec187e6c5
Shivansh Singh <shivanshsingh9670@gmail.com>
2024-07-31 07:55:38 +0530

    View all Contacts

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -6,17 +6,20 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.smart.dao.ContactRepository;
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
@@ -29,6 +32,9 @@ import jakarta.servlet.http.HttpSession;
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ContactRepository contactRepository;
 
     // this will be available for all the pages
     // response method for adding common data
@@ -97,5 +103,21 @@ public class UserController {
         }
 
         return "normal/add_contact_form";
+    }
+
+    // show contacts handler
+    @GetMapping("/show-contacts")
+    public String showContacts(Model model, Principal principal) {
+        model.addAttribute("title", "User Contacts");
+
+        String userName = principal.getName();
+        User user = this.userRepository.getUserByUserName(userName);
+        this.contactRepository.findContactsByUser(user.getId());
+
+        List<Contact> contacts = this.contactRepository.findContactsByUser(user.getId());
+
+        model.addAttribute("contacts", contacts);
+
+        return "normal/show_contacts";
     }
 }
